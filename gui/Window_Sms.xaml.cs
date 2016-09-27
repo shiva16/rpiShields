c503d8e236896502efdde80a4ae658b5000e058c
Metin KOC <saucompeng@gmail.com>
2016-09-27 09:29:47 +0300

    Window_Sms fixed

--- gui/Window_Sms.xaml.cs
+++ gui/Window_Sms.xaml.cs
@@ -218,222 +218,6 @@ namespace SixFabWpf
 
             workActive = false;
 
-            /*
-                
-
-                int i = 0;
-
-                StringBuilder tmp = new StringBuilder();
-
-                do
-                {
-                    try
-                    {
-                        char c = (char)serialPort.ReadChar();
-
-                        tmp.Append(c);
-
-                        toConsoleReceive(c);
-
-                        string[] list = tmp.ToString().Replace("\r\n", "#").Split('#');
-
-                        if (list.Length == 3)
-                        {
-                            if (list[1] == "OK")
-                            {
-                                break;
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        string[] list = tmp.ToString().Replace("\r\n", "#").Split('#');
-
-                        Console.Write(tmp.ToString());
-                        tmp.Clear();
-
-                        if (list.Length == 1)
-                        {
-                            setLabelText("Check on/off");
-                            return;
-                        }
-                        else if (list.Length == 3)
-                        {
-
-                            if (list[1] != "OK")
-                            {
-                                serialPort.WriteLine("AT");
-                                toConsoleSend("AT");
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-                    }
-                } while (true);
-
-
-
-                tmp.Clear();
-                i = 0;
-                serialPort.WriteLine("AT+CMGF=1");
-                toConsoleSend("AT+CMGF=1");
-
-
-                do
-                {
-                    try
-                    {
-                        char c = (char)serialPort.ReadChar();
-
-                        tmp.Append(c);
-
-                        toConsoleReceive(c);
-
-                        string[] list = tmp.ToString().Replace("\r\n", "#").Split('#');
-
-                        if (list.Length == 3)
-                        {
-                            if (list[1] == "OK")
-                            {
-                                break;
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        string[] list = tmp.ToString().Replace("\r\n", "#").Split('#');
-
-                        Console.Write(tmp.ToString());
-                        tmp.Clear();
-
-                        if (list[1] != "OK")
-                        {
-                            serialPort.WriteLine("AT+CMGF=1");
-                            toConsoleSend("AT+CMGF=1");
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                } while (true);
-
-
-
-
-                tmp.Clear();
-                i = 0;
-                serialPort.WriteLine("AT+CSCS=\"GSM\"");
-                toConsoleSend("AT+CSCS=\"GSM\"");
-
-
-                do
-                {
-                    try
-                    {
-                        char c = (char)serialPort.ReadChar();
-
-                        tmp.Append(c);
-
-                        toConsoleReceive(c);
-
-                        string[] list = tmp.ToString().Replace("\r\n", "#").Split('#');
-
-                        if (list.Length == 3)
-                        {
-                            if (list[1] == "OK")
-                            {
-                                break;
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        string[] list = tmp.ToString().Replace("\r\n", "#").Split('#');
-
-                        Console.Write(tmp.ToString());
-                        tmp.Clear();
-
-                        if (list[1] != "OK")
-                        {
-                            serialPort.WriteLine("AT+CSCS=\"GSM\"");
-                            toConsoleSend("AT+CSCS=\"GSM\"");
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                } while (true);
-
-
-
-                tmp.Clear();
-                i = 0;
-
-
-                this.Dispatcher.Invoke((Action)(() =>
-                {
-                    serialPort.WriteLine("AT+CMGS=\"" + PhoneNumber.Text + "\"");
-                    toConsoleSend("AT+CMGS=\"" + PhoneNumber.Text+"\"");
-                }));
-               
-
-
-                do
-                {
-                    try
-                    {
-                        char c = (char)serialPort.ReadChar();
-
-                        tmp.Append(c);
-
-                        toConsoleReceive(c);
-
-                        string[] list = tmp.ToString().Replace("\r\n", "#").Split('#');
-
-                        if (c=='>')
-                        {
-                            serialPort.WriteLine(SmsText.Text.ToString() + "\x1A");
-                            toConsoleSend(SmsText.Text.ToString());
-                            
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        string[] list = tmp.ToString().Replace("\r\n", "#").Split('#');
-
-                        Console.Write(tmp.ToString());
-                        tmp.Clear();
-
-                        if (list.Length > 4)
-                        {
-                            if (list[4] != "OK")
-                            {
-                                serialPort.WriteLine(SmsText.Text.ToString() + "\x1A");
-                                toConsoleSend(SmsText.Text.ToString());
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                } while (true);
-
-
-            }
-            catch (Exception ex)
-            {
-                setLabelText(ex.Message);
-            }
-             * */
         }
 
         private void toConsoleSend(string s)
@@ -493,6 +277,21 @@ namespace SixFabWpf
         private void ClearAllConsoleSend(object sender, RoutedEventArgs e)
         {
             ConsoleSend.Clear();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                setLabelText(ex.Message);
+            }
         }
     }
 }
