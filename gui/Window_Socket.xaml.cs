b0b01e580ceb3d451961bbc50875678fe3b2a873
Metin KOC <saucompeng@gmail.com>
2016-09-30 09:29:35 +0300

    Window_Socket revision

---
+++ gui/Window_Socket.xaml.cs
@@ -0,0 +1,462 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SixFabWpf
+{
+    /// <summary>
+    /// Interaction logic for Window_TcpUdp.xaml
+    /// </summary>
+    public partial class Window_Socket : Window
+    {
+        private SerialPort serialPort;
+        private BackgroundWorker backgroundWorker_ConnectToTcpServer;
+        private StringBuilder buffer;
+        private bool workActive = false;
+        private int waitCounter = 0;
+        private int waitCount = 2;
+
+        public Window_Socket()
+        {
+            InitializeComponent();
+
+            buffer = new StringBuilder();
+
+            serialPort = new SerialPort("COM7", 115200);
+            serialPort.DataReceived += serialPort_DataReceived;
+            serialPort.ReadTimeout = 1500;
+
+
+
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            MouseDown += MainWindow_MouseDown;
+
+        }
+
+        void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!workActive)
+            {
+                toConsoleReceive(serialPort.ReadExisting());
+            }
+        }
+
+        void SendToSerial(string data)
+        {
+            try
+            {
+                toConsoleSend(data + "\r\n");
+                serialPort.Write(data + "\r\n");
+
+            }
+            catch (Exception ex)
+            {
+                setLabelText(ex.Message);
+            }
+        }
+
+        private bool WaitFor(string data)
+        {
+            waitCounter = 0;
+
+            while (true)
+            {
+                try
+                {
+                    string s = ((char)serialPort.ReadChar()).ToString();
+                    buffer.Append(s);
+                    toConsoleReceive(s);
+
+                    if (buffer.ToString().IndexOf(data) > -1)
+                    {
+                        buffer.Clear();
+                        serialPort.ReadExisting();
+                        return true;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    if (++waitCounter > waitCount)
+                    {
+                        workActive = false;
+                        return false;
+                    }
+                }
+            }
+        }
+
+        private void toConsoleSend(string s)
+        {
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                ConsoleSend.Text += s;
+                ConsoleSend.Text += "\r\n";
+                ConsoleSend.ScrollToEnd();
+            }));
+        }
+
+        private void setLabelText(string s)
+        {
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                LblMessage.Content = s;
+            }));
+        }
+
+        private void toConsoleReceive(string s)
+        {
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                ConsoleReceive.Text += s;
+                ConsoleReceive.ScrollToEnd();
+            }));
+        }
+
+        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                    DragMove();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void MouseEnter_Label(object sender, MouseEventArgs e)
+        {
+            ((Label)sender).Foreground = Brushes.White;
+        }
+
+        private void MouseLeave_Label(object sender, MouseEventArgs e)
+        {
+            ((Label)sender).Foreground = Brushes.OrangeRed;
+        }
+
+        private void CloseLabel_Click(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (workActive)
+            {
+                LblMessage.Content = "Process not finished!";
+                return;
+            }
+
+            LblMessage.Content = "Process is running!";
+
+            try
+            {
+                if (!serialPort.IsOpen)
+                {
+                    serialPort.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                setLabelText(ex.Message);
+                return;
+            }
+
+            workActive = true;
+
+            backgroundWorker_ConnectToTcpServer = new BackgroundWorker();
+            backgroundWorker_ConnectToTcpServer.DoWork += backgroundWorker_ConnectToTcpServer_DoWork;
+            backgroundWorker_ConnectToTcpServer.RunWorkerAsync();
+        }
+
+        void backgroundWorker_ConnectToTcpServer_DoWork(object sender, DoWorkEventArgs e)
+        {
+            setLabelText("");
+
+            do
+            {
+                SendToSerial("AT");
+
+                if (WaitFor("OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Check On/Off");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+            waitCount = 10;
+
+            do
+            {
+                SendToSerial("AT+QIDEACT");
+
+                if (WaitFor("DEACT OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+            waitCount = 2;
+
+            do
+            {
+                SendToSerial("AT+QIMODE=0");
+
+                if (WaitFor("OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+            do
+            {
+                SendToSerial("AT+QICSGP=1,\"INTERNET\"");
+
+                if (WaitFor("OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+            do
+            {
+                SendToSerial("AT+QIREGAPP");
+
+                if (WaitFor("OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+            do
+            {
+                SendToSerial("AT+QICSGP?");
+
+                if (WaitFor("+QICSGP: 1"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+            do
+            {
+                SendToSerial("AT+QIACT");
+
+                if (WaitFor("OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+
+            do
+            {
+                SendToSerial("ATV1");
+
+                if (WaitFor("OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+            do
+            {
+                SendToSerial("AT+QIHEAD=1");
+
+                if (WaitFor("OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+
+            do
+            {
+                SendToSerial("AT+QIDNSIP=0");
+
+                if (WaitFor("OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+
+            do
+            {
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+                    SendToSerial("AT+QIOPEN=\"TCP\",\""+Ip.Text+"\",\""+Port.Text+"\"");
+                }));
+                
+
+                if (WaitFor("CONNECT OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+            do
+            {
+                SendToSerial("AT+QISEND");
+
+                if (WaitFor(">"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+
+            do
+            {
+                SendToSerial("Deneme\x1A");
+
+                if (WaitFor("OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+            waitCount = 10;
+
+            do
+            {
+                SendToSerial("AT+QIDEACT");
+
+                if (WaitFor("DEACT OK\r\n"))
+                {
+                    break;
+                }
+                else
+                {
+                    setLabelText("Error, Try Again!");
+                    workActive = false;
+                    return;
+                }
+            } while (true);
+
+            waitCount = 2;
+
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                LblMessage.Content = "Process Finished!";
+            }));
+
+            workActive = false;
+        }
+
+        private void ClearAllConsoleReceive(object sender, RoutedEventArgs e)
+        {
+            ConsoleReceive.Clear();
+        }
+
+        private void ClearAllConsoleSend(object sender, RoutedEventArgs e)
+        {
+            ConsoleSend.Clear();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                setLabelText(ex.Message);
+            }
+        }
+    }
+}
